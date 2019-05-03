fc2c6ee369fef9abf2ffa53a8b61f2a5fb0c1f62
Robert Mikhayelyan <rob.mikh@outlook.com>
2019-05-03 16:03:54 -0700

    Add project files.

---
+++ VisualCaptureDemo/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Graphics.Canvas;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics;
+using Windows.Graphics.Capture;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace VisualCaptureDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _device = new CanvasDevice();
+        }
+
+        private void MainWebView_Loaded(object sender, RoutedEventArgs e)
+        {
+            var compositor = Window.Current.Compositor;
+            var visual = ElementCompositionPreview.GetElementVisual(MainWebView);
+
+            var size = new SizeInt32()
+            {
+                Width = (int)MainWebView.ActualWidth,
+                Height = (int)MainWebView.ActualHeight
+            };
+
+            var item = GraphicsCaptureItem.CreateFromVisual(visual);
+            _capture = new SimpleCapture(_device, item, size);
+            var surface = _capture.CreateSurface(compositor);
+
+            _visual = compositor.CreateSpriteVisual();
+            _visual.RelativeSizeAdjustment = Vector2.One;
+            _visual.Brush = compositor.CreateSurfaceBrush(surface);
+
+            ElementCompositionPreview.SetElementChildVisual(VisualGrid, _visual);
+            _capture.StartCapture();
+        }
+
+        private CanvasDevice _device;
+        private SimpleCapture _capture;
+        private SpriteVisual _visual;
+    }
+}
---
+++ VisualCaptureDemo/SimpleCapture.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI.Composition;
+using System;
+using Windows.Graphics;
+using Windows.Graphics.Capture;
+using Windows.Graphics.DirectX;
+using Windows.UI;
+using Windows.UI.Composition;
+
+namespace VisualCaptureDemo
+{
+    class SimpleCapture : IDisposable
+    {
+        public SimpleCapture(CanvasDevice device, GraphicsCaptureItem item, SizeInt32 size)
+        {
+            _item = item;
+            _device = device;
+
+            // TODO: Dpi?
+            _swapChain = new CanvasSwapChain(_device, size.Width, size.Height, 96);
+
+            _framePool = Direct3D11CaptureFramePool.CreateFreeThreaded(
+                    _device,
+                    DirectXPixelFormat.B8G8R8A8UIntNormalized,
+                    2,
+                    size);
+            _session = _framePool.CreateCaptureSession(item);
+
+            _framePool.FrameArrived += OnFrameArrived;
+        }
+
+        public void StartCapture()
+        {
+            _session.StartCapture();
+        }
+
+        public ICompositionSurface CreateSurface(Compositor compositor)
+        {
+            return CanvasComposition.CreateCompositionSurfaceForSwapChain(compositor, _swapChain);
+        }
+
+        public void Dispose()
+        {
+            _session?.Dispose();
+            _framePool?.Dispose();
+            _swapChain?.Dispose();
+
+            _swapChain = null;
+            _framePool = null;
+            _session = null;
+            _item = null;
+        }
+
+        private void OnFrameArrived(Direct3D11CaptureFramePool sender, object args)
+        {
+            using (var frame = sender.TryGetNextFrame())
+            {
+                using (var bitmap = CanvasBitmap.CreateFromDirect3D11Surface(_device, frame.Surface))
+                using (var drawingSession = _swapChain.CreateDrawingSession(Colors.Transparent))
+                {
+                    drawingSession.DrawImage(bitmap);
+                }
+
+            } // retire the frame
+
+            _swapChain.Present();
+        }
+
+        private GraphicsCaptureItem _item;
+        private Direct3D11CaptureFramePool _framePool;
+        private GraphicsCaptureSession _session;
+
+        private CanvasDevice _device;
+        private CanvasSwapChain _swapChain;
+    }
+}
